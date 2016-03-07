5b810d2c10936c1dc81c73f87acb1831e4ae5fd2
Etoga <vcerja00@fesb.hr>
2016-03-07 05:41:11 +0100

    Hello message added

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
         }
     }
 }
